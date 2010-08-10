40e82fb5b32596f06d1d7539a60d0306b36bebd9
ben.loftin <ben.loftin@450ed9c9-de5e-6098-e474-0b90b4c4b999>
2010-08-10 14:20:33 +0000

    this will record the gyro data to a file, needs the atmega/arduino

---
+++ trunk/MechWarrior/FormGyroRecord/FormGyroRecord/FormGyroRecord.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace FormGyroRecord
+{
+    partial class FormRecordGyro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonOutputFile = new System.Windows.Forms.Button();
+            this.textBoxOutputFile = new System.Windows.Forms.TextBox();
+            this.saveOutputFile = new System.Windows.Forms.SaveFileDialog();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.textBoxDebug = new System.Windows.Forms.TextBox();
+            this.labelDebugLog = new System.Windows.Forms.Label();
+            this.serialPortArduino = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // buttonOutputFile
+            // 
+            this.buttonOutputFile.Location = new System.Drawing.Point(12, 33);
+            this.buttonOutputFile.Name = "buttonOutputFile";
+            this.buttonOutputFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonOutputFile.TabIndex = 0;
+            this.buttonOutputFile.Text = "Outupt File";
+            this.buttonOutputFile.UseVisualStyleBackColor = true;
+            this.buttonOutputFile.Click += new System.EventHandler(this.buttonOutputFile_Click);
+            // 
+            // textBoxOutputFile
+            // 
+            this.textBoxOutputFile.Location = new System.Drawing.Point(93, 34);
+            this.textBoxOutputFile.Name = "textBoxOutputFile";
+            this.textBoxOutputFile.Size = new System.Drawing.Size(248, 20);
+            this.textBoxOutputFile.TabIndex = 1;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 91);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(266, 91);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 3;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // textBoxDebug
+            // 
+            this.textBoxDebug.Location = new System.Drawing.Point(14, 148);
+            this.textBoxDebug.Multiline = true;
+            this.textBoxDebug.Name = "textBoxDebug";
+            this.textBoxDebug.Size = new System.Drawing.Size(326, 82);
+            this.textBoxDebug.TabIndex = 4;
+            // 
+            // labelDebugLog
+            // 
+            this.labelDebugLog.AutoSize = true;
+            this.labelDebugLog.Location = new System.Drawing.Point(14, 129);
+            this.labelDebugLog.Name = "labelDebugLog";
+            this.labelDebugLog.Size = new System.Drawing.Size(60, 13);
+            this.labelDebugLog.TabIndex = 5;
+            this.labelDebugLog.Text = "Debug Log";
+            // 
+            // serialPortArduino
+            // 
+            this.serialPortArduino.BaudRate = 115200;
+            this.serialPortArduino.PortName = "COM8";
+            this.serialPortArduino.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPortArduino_DataReceived);
+            // 
+            // FormRecordGyro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 246);
+            this.Controls.Add(this.labelDebugLog);
+            this.Controls.Add(this.textBoxDebug);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxOutputFile);
+            this.Controls.Add(this.buttonOutputFile);
+            this.Name = "FormRecordGyro";
+            this.Text = "Gyro Record";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormRecordGyro_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOutputFile;
+        private System.Windows.Forms.TextBox textBoxOutputFile;
+        private System.Windows.Forms.SaveFileDialog saveOutputFile;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.TextBox textBoxDebug;
+        private System.Windows.Forms.Label labelDebugLog;
+        private System.IO.Ports.SerialPort serialPortArduino;
+    }
+}
+
---
+++ trunk/MechWarrior/FormGyroRecord/FormGyroRecord/FormGyroRecord.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace FormGyroRecord
+{
+    public partial class FormRecordGyro : Form
+    {
+        ASCIIEncoding ascii;
+        StreamWriter fileOut;
+        DateTime CurrTime;
+        int rawGyroValue = 0;
+
+        public FormRecordGyro()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOutputFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBoxOutputFile.Text = saveFileDialog1.FileName;
+            } 
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            // Open Serial Port
+            try
+            {
+                
+
+                // open file to write
+                if (textBoxOutputFile.Text == String.Empty)
+                {
+                     SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+                    saveFileDialog1.RestoreDirectory = true;
+
+                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        textBoxOutputFile.Text = saveFileDialog1.FileName;
+                    }
+                }
+
+                fileOut = new StreamWriter(textBoxOutputFile.Text);
+
+                // write Date/time for first line
+                CurrTime = DateTime.Now;
+
+                fileOut.WriteLine("% " + CurrTime.Date.ToLongDateString());
+                fileOut.WriteLine("% Timestamp, Analog Value (0-1023)");
+                
+                // disable the button while recording, enable the stop
+                buttonStart.Enabled = false;
+                buttonStop.Enabled = true;
+
+                // The encoding, ardunio uses ASCII encoding in serial comm
+                ascii = new ASCIIEncoding();
+
+                serialPortArduino.Open();
+                serialPortArduino.Encoding = ascii;
+            }
+            catch (Exception ex)
+            {
+                if (serialPortArduino.IsOpen)
+                {
+                    serialPortArduino.Close();
+                }
+
+                if (fileOut != null)
+                {
+                    fileOut.Close();
+                }
+
+                textBoxDebug.AppendText(ex.Message.ToString());
+            }
+
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            if (serialPortArduino.IsOpen)
+            {
+                serialPortArduino.Close();
+            }
+
+            if (fileOut != null)
+            {
+                fileOut.Close();
+            }
+
+            // disable the button while recording, enable the stop
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = false;
+            
+        }
+
+        private void serialPortArduino_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                CurrTime = DateTime.Now;
+                string str = serialPortArduino.ReadLine();
+                
+                str = str.Replace("\r", "");
+                
+                // build up time stamp
+                string time = CurrTime.Hour.ToString() + ":" + CurrTime.Minute.ToString() + ":" +
+                              CurrTime.Second.ToString() + "." + CurrTime.Millisecond.ToString() + ",";
+                fileOut.WriteLine( time + str );
+             
+            }
+            catch (Exception ex)
+            {
+                if (serialPortArduino.IsOpen)
+                {
+                    serialPortArduino.Close();
+                }
+
+                textBoxDebug.AppendText(ex.Message.ToString());
+            }
+        }
+
+        private void FormRecordGyro_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPortArduino.IsOpen)
+            {
+                serialPortArduino.Close();
+            }
+            if (fileOut != null)
+            {
+                fileOut.Close();
+            }
+        }
+    }
+}

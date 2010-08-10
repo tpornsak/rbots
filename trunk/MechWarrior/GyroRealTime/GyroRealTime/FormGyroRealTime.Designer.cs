9ee658a925a2c00b337f48976aa853b9552df31d
ben.loftin <ben.loftin@450ed9c9-de5e-6098-e474-0b90b4c4b999>
2010-08-10 14:19:09 +0000

    real time plot of A2D gyro data

---
+++ trunk/MechWarrior/GyroRealTime/GyroRealTime/FormGyroRealTime.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace GyroRealTime
+{
+    partial class FormGyroRealTime
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBoxDebug = new System.Windows.Forms.TextBox();
+            this.serialPortArduino = new System.IO.Ports.SerialPort(this.components);
+            this.labelGyroPlot = new System.Windows.Forms.Label();
+            this.labelDebug = new System.Windows.Forms.Label();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxDebug
+            // 
+            this.textBoxDebug.Location = new System.Drawing.Point(12, 80);
+            this.textBoxDebug.Multiline = true;
+            this.textBoxDebug.Name = "textBoxDebug";
+            this.textBoxDebug.Size = new System.Drawing.Size(177, 400);
+            this.textBoxDebug.TabIndex = 4;
+            // 
+            // serialPortArduino
+            // 
+            this.serialPortArduino.BaudRate = 115200;
+            this.serialPortArduino.PortName = "COM8";
+            this.serialPortArduino.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPortArduino_DataReceived);
+            // 
+            // labelGyroPlot
+            // 
+            this.labelGyroPlot.AutoSize = true;
+            this.labelGyroPlot.Location = new System.Drawing.Point(204, 53);
+            this.labelGyroPlot.Name = "labelGyroPlot";
+            this.labelGyroPlot.Size = new System.Drawing.Size(75, 13);
+            this.labelGyroPlot.TabIndex = 35;
+            this.labelGyroPlot.Text = "Gyro Raw Plot";
+            // 
+            // labelDebug
+            // 
+            this.labelDebug.AutoSize = true;
+            this.labelDebug.Location = new System.Drawing.Point(12, 53);
+            this.labelDebug.Name = "labelDebug";
+            this.labelDebug.Size = new System.Drawing.Size(39, 13);
+            this.labelDebug.TabIndex = 36;
+            this.labelDebug.Text = "Debug";
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(15, 12);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 37;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // FormGyroRealTime
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 509);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.labelDebug);
+            this.Controls.Add(this.labelGyroPlot);
+            this.Controls.Add(this.textBoxDebug);
+            this.Name = "FormGyroRealTime";
+            this.Text = "Gyro Real Time";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormGyroRealTime_FormClosing);
+            this.Load += new System.EventHandler(this.FormGyroRealTime_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormGyroRealTime_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxDebug;
+        private System.IO.Ports.SerialPort serialPortArduino;
+        private System.Windows.Forms.Label labelGyroPlot;
+        private System.Windows.Forms.Label labelDebug;
+        private System.Windows.Forms.Button buttonClose;
+    }
+}
+
---
+++ trunk/MechWarrior/GyroRealTime/GyroRealTime/FormGyroRealTime.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GyroRealTime
+{
+    public partial class FormGyroRealTime : Form
+    {
+        ASCIIEncoding ascii;
+        const int rawGridMaxX = 800;
+        const int spaceX = 5;
+        const int rawGridMaxY = 480;
+        const int rawGridMinX = 200;
+        const int rawGridMinY = 80;
+        const int analogRange = 1024;
+        int currentX = 0;
+        float[] dashValuesGrid = { 10, 10};
+        const int bufferSizeX = (rawGridMaxX-rawGridMinX)/spaceX;
+        int[] bufferX = new int[bufferSizeX];
+        int currentBuffer = 0;
+        int rawGyroValue = 0;
+
+
+        public FormGyroRealTime()
+        {
+            InitializeComponent();
+            InitializeSerial();
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            this.SetStyle(ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer, true);
+
+            
+        }
+
+        private void InitializeSerial()
+        {
+
+
+            try
+            {
+                // The encoding, ardunio uses ASCII encoding in serial comm
+                ascii = new ASCIIEncoding();
+
+                serialPortArduino.Open();
+                serialPortArduino.Encoding = ascii;
+
+            }
+            catch (Exception ex)
+            {
+                if (serialPortArduino.IsOpen)
+                {
+                    serialPortArduino.Close();
+                }
+
+                textBoxDebug.AppendText(ex.Message.ToString());
+            }
+        }
+
+        public void RefreshPlot()
+        {
+            this.Refresh();
+        }
+
+        private void serialPortArduino_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string str = serialPortArduino.ReadLine();
+                
+                try
+                {
+                    if (str != null)
+                    {
+                        rawGyroValue = int.Parse(str);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    rawGyroValue = 0;
+                }
+                bufferX[currentBuffer] = rawGyroValue;
+                currentBuffer += 1;
+                if (currentBuffer >= bufferSizeX)
+                {
+                    currentBuffer = 0;
+                    serialPortArduino.DiscardInBuffer();
+                }
+
+                this.Refresh();
+                
+            }
+            catch (Exception ex)
+            {
+                if (serialPortArduino.IsOpen)
+                {
+                    serialPortArduino.Close();
+                }
+
+                textBoxDebug.AppendText(ex.Message.ToString());
+            }
+
+        }
+
+        private void FormGyroRealTime_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            if (serialPortArduino.IsOpen)
+            {
+                serialPortArduino.Close();
+            }
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+           
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            if (serialPortArduino.IsOpen)
+            {
+                serialPortArduino.Close();
+            }
+        }
+
+        private void FormGyroRealTime_Paint(object sender, PaintEventArgs e)
+        {
+            try
+            {
+                Graphics g;
+                Bitmap drawing = null;
+
+                drawing = new Bitmap(this.Width, this.Height, e.Graphics);
+                g = Graphics.FromImage(drawing);
+
+                // Draw the grid
+                Pen gridPen = new Pen(Color.Black);
+                gridPen.DashPattern = dashValuesGrid;
+                // y axis
+                for (int x = rawGridMinX; x <= rawGridMaxX; x += 100)
+                {
+                    g.DrawLine(gridPen, x, rawGridMaxY, x, rawGridMinY);
+                }
+
+                // x axis
+                for (int y = rawGridMinY; y <= rawGridMaxY; y += 100)
+                {
+                    g.DrawLine(gridPen, rawGridMinX, y, rawGridMaxX, y);
+                }
+
+                // zero on y axis is (rawGridMaxY - rawGridMinY)/2
+                float scale = (float)(rawGridMaxY - rawGridMinY) / (float)analogRange;
+                for (int x = 0; x <= currentBuffer; x++)
+                {
+                    g.DrawEllipse(new Pen(Color.Blue), new Rectangle(currentX, (int)(bufferX[x] * scale) + rawGridMinY, 5, 5));
+                    currentX += 5;
+
+                    if (currentX > rawGridMaxX)
+                    {
+                        currentX = rawGridMinX;
+                    }
+                }
+
+                e.Graphics.DrawImageUnscaled(drawing, 0, 0);
+                g.Dispose();
+                
+            }
+            catch (Exception ex)
+            {
+                textBoxDebug.AppendText(ex.Message.ToString());
+            }
+        }
+
+        private void FormGyroRealTime_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
